bebf5ce762615d1ddf71c1b88d45bdc8e28c3730
mukhtharcm <mukhtharcm@gmail.com>
2021-08-08 23:48:13 +0530

    sync

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,31 @@
 module.exports = {
-  purge: [
-    './public/*.html'
-  ],
+  purge: ["./public/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "foreignwind-purple": "#5267DF",
+        "foreignwind-red": "#FA5959",
+        "foreignwind-blue": "#242A45",
+        "foreignwind-grey": "#9194A2",
+        "foreignwind-white": "#f7f7f7",
+      },
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
+  ],
+};
